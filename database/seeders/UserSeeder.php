9d16780280dd7c8f9b12358c9cbba300fd245ef8
GasyCoder <gasikaradigital@gmail.com>
2025-03-25 17:30:08 +0300

    fixer login and password

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,11 +4,20 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        User::factory()->count(2)->create();
+        User::where('id', '>', 0)->delete();
+
+        // admin
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@pirata.fr',
+            'password' => Hash::make('admin+123'),
+            'email_verified_at' => now(),
+        ]);
     }
 }
